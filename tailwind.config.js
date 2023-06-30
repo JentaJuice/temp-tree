b8170fb50b2aad6a1713c939bf089a9bef14cef7
Suphakit P <me@suphakit.net>
2023-07-01 04:09:23 +0700

    chore<config>: tailwind config extended

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      'pastel-black': '#2e2f2f',
+      'whitesmoke': '#f5f5f5',
+      'living-coral': '#fe6e61',
+      'facebook': '#3a5998',
+      'twitter': '#1C9BEF',
+      'instagram': '#AC21D2',
+      'overlay': 'rgba(0,0,0,0.4)',
+    },
+    borderRadius: {
+      DEFAULT: '1rem'
+    },
+    extend: {
+      scale: {
+        '85': '.85'
+      },
+    },
   },
   plugins: [],
 }
