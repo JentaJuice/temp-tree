cdf3ec9fcdd7be91205f3cbffe062b4894b9da12
Suphakit P <me@suphakit.net>
2023-07-01 13:57:00 +0700

    feat<db>: miss me yet various edition

---
+++ src/pages/trees/kat.wav/07-miss-me-yet-various/data.ts
@@ -0,0 +1,37 @@
+import DataInterface from "../../interface"
+
+const data: DataInterface = {
+  title: 'miss me yet? but various version :D ',
+  artist: 'katsuragi.wav',
+  holder: '2022 JentaJuice',
+  cover: 'https://s3.amazonaws.com/gather.fandalism.com/3000x3000-2978870--B90E6362-4A59-4486-9F2CA546392BA29E--0--237414--IMG0086.jpg',
+  links: [
+    {
+      id: 0,
+      service: 'youtube',
+      link: 'https://www.youtube.com/playlist?list=OLAK5uy_mj86WDRPN3AEmjBmkZzmEQCJX28xatLs4',
+    },
+    {
+      id: 1,
+      service: 'ytmusic',
+      link: 'https://music.youtube.com/playlist?list=OLAK5uy_lbv8yqhgLo1GftZoomFTwadTE4DsuLoAY',
+    },
+    {
+      id: 2,
+      service: 'spotify',
+      link: 'https://open.spotify.com/album/4rOEzRSQonCms7TIytAorb',
+    },
+    {
+      id: 3,
+      service: 'itunes',
+      link: 'https://music.apple.com/th/album/miss-me-yet-but-various-version-d-ep/1657522834',
+    },
+    {
+      id: 4,
+      service: 'tidal',
+      link: 'https://listen.tidal.com/album/263872372',
+    },
+  ],
+}
+
+export default data
