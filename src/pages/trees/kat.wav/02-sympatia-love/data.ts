f0f110adb9de8a7eaebd36415c849604a391c793
Suphakit P <me@suphakit.net>
2023-07-02 00:15:36 +0700

    feat<db>: sympatia love

---
+++ src/pages/trees/kat.wav/02-sympatia-love/data.ts
@@ -0,0 +1,37 @@
+import DataInterface from "../../interface"
+
+const data: DataInterface = {
+  title: 'Sympatia Love',
+  artist: 'katsuragi.wav',
+  holder: '2021 JentaJuice',
+  cover: 'https://s3.amazonaws.com/gather.fandalism.com/3000x3000-2978870--0C95206B-5958-423C-B11BE5AAE25EB42D--1626281886919--Untitled96.jpg',
+  links: [
+    {
+      id: 0,
+      service: 'youtube',
+      link: 'https://www.youtube.com/watch?v=MI0RbBtNem4',
+    },
+    {
+      id: 1,
+      service: 'ytmusic',
+      link: 'https://music.youtube.com/watch?v=MI0RbBtNem4',
+    },
+    {
+      id: 2,
+      service: 'spotify',
+      link: 'https://open.spotify.com/album/46dyL2WhBnJZQLA8ChhtUu?si=4xkjqXInT8SuCU0T-LYDbg&dl_branch=1',
+    },
+    {
+      id: 3,
+      service: 'itunes',
+      link: 'https://music.apple.com/sg/album/sympatia-love-single/1576962279',
+    },
+    {
+      id: 4,
+      service: 'tidal',
+      link: 'https://tidal.com/browse/album/191303223',
+    },
+  ],
+}
+
+export default data
