59b0601632241a893f2bc3228a72e0809c5ae391
Suphakit P <me@suphakit.net>
2023-07-02 00:26:53 +0700

    feat<db>: pastel delightful

---
+++ src/pages/trees/kat.wav/06-pastel-delightful/data.ts
@@ -0,0 +1,37 @@
+import DataInterface from "../../interface"
+
+const data: DataInterface = {
+  title: 'Pastel Delightful',
+  artist: 'katsuragi.wav',
+  holder: '2022 JentaJuice',
+  cover: 'https://s3.amazonaws.com/gather.fandalism.com/3000x3000-2978870--31B70850-6450-4CE8-B21A51716CC8A347--0--329137--detmonoday.jpg',
+  links: [
+    {
+      id: 0,
+      service: 'youtube',
+      link: 'https://www.youtube.com/playlist?list=OLAK5uy_nkQXOsMzdh1-6p9xhOcw6ODngxuys-CwU',
+    },
+    {
+      id: 1,
+      service: 'ytmusic',
+      link: 'https://music.youtube.com/playlist?list=OLAK5uy_nuizULgQTaFym0YwvnGwghpEBLEodZeGM',
+    },
+    {
+      id: 2,
+      service: 'spotify',
+      link: 'https://open.spotify.com/album/3it4qE5D0WINsptqbJdc40?si=yUjWYZHfTOmWDq84fiOsCA',
+    },
+    {
+      id: 3,
+      service: 'itunes',
+      link: '',
+    },
+    {
+      id: 4,
+      service: 'tidal',
+      link: 'https://tidal.com/browse/album/278306718',
+    },
+  ],
+}
+
+export default data
