6652c0a57f29759e7a4e06b8e6bc623d3f57109d
Suphakit P <me@suphakit.net>
2023-07-01 13:58:37 +0700

    feat<db>: With U

---
+++ src/pages/trees/kat.wav/08-with-u/data.ts
@@ -0,0 +1,37 @@
+import DataInterface from "../../interface"
+
+const data: DataInterface = {
+  title: 'With U (ft. กบ VTuberTH)',
+  artist: 'katsuragi.wav',
+  holder: '2023 JentaJuice',
+  cover: 'https://is2-ssl.mzstatic.com/image/thumb/Music126/v4/9e/0a/39/9e0a3981-8b21-2ecf-3c92-d1662d354d93/5063161846134_cover.jpg/3000x3000bb.webp',
+  links: [
+    {
+      id: 0,
+      service: 'youtube',
+      link: 'https://youtu.be/YM1gUvsPa-Y',
+    },
+    {
+      id: 1,
+      service: 'ytmusic',
+      link: 'https://music.youtube.com/playlist?list=OLAK5uy_m2a0kkXugfmA52CR0ygOAmeMPF4Q-HeMc',
+    },
+    {
+      id: 2,
+      service: 'spotify',
+      link: 'https://open.spotify.com/album/64bLdG1P2AL0HQd64xqhyL',
+    },
+    {
+      id: 3,
+      service: 'itunes',
+      link: 'https://music.apple.com/th/album/with-u-feat-%E0%B8%81%E0%B8%9A-vtuberth-single/1671564714',
+    },
+    {
+      id: 4,
+      service: 'tidal',
+      link: 'https://listen.tidal.com/album/276475176',
+    },
+  ],
+}
+
+export default data
