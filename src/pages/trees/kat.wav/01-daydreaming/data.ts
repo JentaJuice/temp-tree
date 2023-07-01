2a565b7bc078d89e92b4e45d9681bd8bb7ea6039
Suphakit P <me@suphakit.net>
2023-07-02 00:12:54 +0700

    feat<db>: daydreaming

---
+++ src/pages/trees/kat.wav/01-daydreaming/data.ts
@@ -0,0 +1,37 @@
+import DataInterface from "../../interface"
+
+const data: DataInterface = {
+  title: 'Daydreaming',
+  artist: 'katsuragi.wav',
+  holder: '2021 JentaJuice',
+  cover: 'https://s3.amazonaws.com/gather.fandalism.com/3000x3000-2978870--546FA910-BBF0-430C-98DA9B73689AE88B--1635434916665--3000x3000297887052DC8D5FC9844EA6B752D4AB00AC3BF9mod1621650113.jpg',
+  links: [
+    {
+      id: 0,
+      service: 'youtube',
+      link: 'https://youtu.be/5ckvtQqxpuI',
+    },
+    {
+      id: 1,
+      service: 'ytmusic',
+      link: 'https://music.youtube.com/5ckvtQqxpuI',
+    },
+    {
+      id: 2,
+      service: 'spotify',
+      link: 'https://open.spotify.com/album/3yEJgBat2hLxcv207Qhy39',
+    },
+    {
+      id: 3,
+      service: 'itunes',
+      link: 'https://music.apple.com/us/album/daydreaming-single/1592536612',
+    },
+    {
+      id: 4,
+      service: 'tidal',
+      link: 'https://tidal.com/browse/album/202841990',
+    },
+  ],
+}
+
+export default data
