06946dc48c98c9fc222a3a4f3a3706728f5844a9
Suphakit P <me@suphakit.net>
2023-07-02 00:18:58 +0700

    feat<db>: Colorful Illusion

---
+++ src/pages/trees/kat.wav/03-colorful-illusion/data.ts
@@ -0,0 +1,37 @@
+import DataInterface from "../../interface"
+
+const data: DataInterface = {
+  title: 'Colorful Illusion',
+  artist: 'katsuragi.wav',
+  holder: '2021 JentaJuice',
+  cover: 'https://s3.amazonaws.com/gather.fandalism.com/3000x3000-2978870--A6CA87D4-D446-4A4A-9086FB25F16548EA--1638638641191--detcolorfulillus.jpg',
+  links: [
+    {
+      id: 0,
+      service: 'youtube',
+      link: 'https://www.youtube.com/playlist?list=OLAK5uy_mFWOHWBHHLwzQ2W19i9DR9cCPMV0sVq-Y',
+    },
+    {
+      id: 1,
+      service: 'ytmusic',
+      link: 'https://music.youtube.com/playlist?list=OLAK5uy_mFWOHWBHHLwzQ2W19i9DR9cCPMV0sVq-Y',
+    },
+    {
+      id: 2,
+      service: 'spotify',
+      link: 'https://open.spotify.com/album/5vgCewScLzceC0kA2sy7AI?si=gWdCSIIZTMeMsxMn0JmtKA',
+    },
+    {
+      id: 3,
+      service: 'itunes',
+      link: 'https://music.apple.com/us/album/colorful-illusion-ep/1598986477',
+    },
+    {
+      id: 4,
+      service: 'tidal',
+      link: 'https://tidal.com/browse/album/208012842',
+    },
+  ],
+}
+
+export default data
