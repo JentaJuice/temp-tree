5a5631fecae65027196a115cb2158fd8fa5846b0
Suphakit P <me@suphakit.net>
2023-07-02 00:20:57 +0700

    feat<db>: reality

---
+++ src/pages/trees/kat.wav/04-reality/data.ts
@@ -0,0 +1,37 @@
+import DataInterface from "../../interface"
+
+const data: DataInterface = {
+  title: 'Reality.. (ft. Ishijima00)',
+  artist: 'katsuragi.wav',
+  holder: '2022 JentaJuice',
+  cover: 'https://cloudinary-cdn.ffm.to/s--1ZhWRuhu--/f_webp/https%3A%2F%2Fimagestore.ffm.to%2Flink%2F44d959aa991782365dbb4c970e33ab03.jpeg',
+  links: [
+    {
+      id: 0,
+      service: 'youtube',
+      link: 'https://youtube.com/playlist?list=OLAK5uy_kQwloPaIT22G5sJMbmjDbelXqF6p6_st4',
+    },
+    {
+      id: 1,
+      service: 'ytmusic',
+      link: 'https://music.youtube.com/playlist?list=OLAK5uy_kQwloPaIT22G5sJMbmjDbelXqF6p6_st4',
+    },
+    {
+      id: 2,
+      service: 'spotify',
+      link: 'https://open.spotify.com/album/5HljnXT4ei2zuq04Aqdp84',
+    },
+    {
+      id: 3,
+      service: 'itunes',
+      link: 'https://music.apple.com/th/album/reality-feat-ishijima00-single/1666526870',
+    },
+    {
+      id: 4,
+      service: 'tidal',
+      link: 'https://listen.tidal.com/album/272383259',
+    },
+  ],
+}
+
+export default data
