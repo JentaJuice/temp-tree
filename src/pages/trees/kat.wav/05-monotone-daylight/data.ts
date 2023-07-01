e487d1f79194e1e1a00ef5ebe24775dfde2a0c25
Suphakit P <me@suphakit.net>
2023-07-02 00:23:00 +0700

    feat<db>: Monotone Daylight

---
+++ src/pages/trees/kat.wav/05-monotone-daylight/data.ts
@@ -0,0 +1,37 @@
+import DataInterface from "../../interface"
+
+const data: DataInterface = {
+  title: 'Reality.. (ft. Ishijima00)',
+  artist: 'katsuragi.wav',
+  holder: '2022 JentaJuice',
+  cover: 'https://s3.amazonaws.com/gather.fandalism.com/3000x3000-2978870--31B70850-6450-4CE8-B21A51716CC8A347--0--329137--detmonoday.jpg',
+  links: [
+    {
+      id: 0,
+      service: 'youtube',
+      link: 'https://www.youtube.com/playlist?list=PL2hR5KmwM4AirdF-vHuA8PsiwVxNy6x_z',
+    },
+    {
+      id: 1,
+      service: 'ytmusic',
+      link: 'https://music.youtube.com/playlist?list=OLAK5uy_mt6OWC_uOdLz-FbsmS1-hD2AWfv2rJFIA&feature=share',
+    },
+    {
+      id: 2,
+      service: 'spotify',
+      link: 'https://open.spotify.com/album/0M2EnUW1Et3cRSHC9GcX3b',
+    },
+    {
+      id: 3,
+      service: 'itunes',
+      link: 'https://music.apple.com/us/album/monotone-daylight-ep/1629773795?uo=4',
+    },
+    {
+      id: 4,
+      service: 'tidal',
+      link: 'https://tidal.com/browse/album/233714598',
+    },
+  ],
+}
+
+export default data
