52623d49b2c05f09ff15a1da411447f611b40c5e
Suphakit P <me@suphakit.net>
2023-07-02 00:50:11 +0700

    feat<router>: artist tree path

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,9 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 import ROUTES from './constants/route'
 import Layout from './pages/_Layout'
 
 import Home from './pages/home/index'
 import NotFound from './pages/errors/notFound'
+
+import TreeLayout from './pages/trees/pageLayout'
 
 export const Router = () => {
   return (
@@ -11,6 +14,24 @@ export const Router = () => {
       <Layout>
         <Routes>
           <Route path={ROUTES.HOME} element={<Home />} />
+
+          {/* Anxiowaldosiaticist */}
+          {ROUTES.AXIO.paths.map((item) => (
+            <Route
+              key={item.id}
+              path={ROUTES.AXIO.base + item.path}
+              element={<TreeLayout artist="axio" name={item.path} />}
+            />
+          ))}
+
+          {/* katsuragi.wav */}
+          {ROUTES.KATWAV.paths.map((item) => (
+            <Route
+              key={item.id}
+              path={ROUTES.KATWAV.base + item.path}
+              element={<TreeLayout artist="kat-wav" name={item.path} />}
+            />
+          ))}
 
           <Route path="*" element={<NotFound />} />
         </Routes>
