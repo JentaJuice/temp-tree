e2c95353bd5f17201fcf7d60d0d631b338ce6a14
Suphakit P <me@suphakit.net>
2023-07-02 00:46:59 +0700

    feat<components>: footer components

---
+++ src/components/LinkFooter.tsx
@@ -0,0 +1,16 @@
+import className from "../constants/tailwind.class"
+
+const Footer = () => {
+  return (
+    <div className="w-full py-4 flex flex-row fixed bottom-0 bg-pastel-black">
+      <div className="w-full px-4 hidden md:block">
+        made w/ 🤍 by <a href="https://suphakit.net" className={className.LINK}>Suphakit P.</a>
+      </div>
+      <div className="w-full px-4 text-center md:text-right">
+        &copy; 2023 <a href="https://suphakit.net" className={className.LINK}>Suphakit P.</a> All Right Reserved.
+      </div>
+    </div>
+  )
+}
+
+export default Footer
