63814aad1a72a3d3d0dfed8d3f5832c01fcd6241
Suphakit P <me@suphakit.net>
2023-07-01 13:07:11 +0700

    feat<components>: footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,16 @@
+import className from "../constants/tailwind.class"
+
+const Footer = () => {
+  return (
+    <div className="w-full py-4 fixed bottom-0">
+      <p className="py-1">
+        made with 🤍 by <a href="https://suphakit.net" className={className.LINK}>Suphakit P.</a>
+      </p>
+      <p className="py-1">
+        &copy; 2023{(new Date().getFullYear() != 2023) ? ` - new Date().getFullYear()` : ''} <a href="https://suphakit.net" className={className.LINK}>Suphakit P.</a> All Right Reserved.
+      </p>
+    </div>
+  )
+}
+
+export default Footer
