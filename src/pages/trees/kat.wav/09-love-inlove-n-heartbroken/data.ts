77755a95be718ddf6fa41da21188bf0263f9ab8b
Suphakit P <me@suphakit.net>
2023-07-01 14:03:54 +0700

    feat<db>: love, in love n' heartbroken

---
+++ src/pages/trees/kat.wav/09-love-inlove-n-heartbroken/data.ts
@@ -0,0 +1,37 @@
+import DataInterface from "../../interface"
+
+const data: DataInterface = {
+  title: 'Love, In love & Heartbroken',
+  artist: 'katsuragi.wav',
+  holder: '2023 JentaJuice',
+  cover: 'https://s3.amazonaws.com/gather.fandalism.com/3000x3000-2978870--E4503AA0-3A46-4D2E-A1A5255B2030DD33--0--6685678--loveinloveandheartbroken.jpg',
+  links: [
+    {
+      id: 0,
+      service: 'youtube',
+      link: 'https://www.youtube.com/playlist?list=PL2hR5KmwM4AiURV2Tp1ZiaUlkosuXP0Sm',
+    },
+    {
+      id: 1,
+      service: 'ytmusic',
+      link: 'https://music.youtube.com/playlist?list=OLAK5uy_kmYd4QEUtiQvX1rY_2g06tFs2bwRtcgzA',
+    },
+    {
+      id: 2,
+      service: 'spotify',
+      link: 'https://open.spotify.com/album/1yqM70rErl14hxzfCdwPY2?si=Ze3jfvqCSaqquE8wtgyMDQ',
+    },
+    {
+      id: 3,
+      service: 'itunes',
+      link: 'https://music.apple.com/th/album/love-in-love-heartbroken-ep/1692839564',
+    },
+    {
+      id: 4,
+      service: 'tidal',
+      link: 'https://tidal.com/browse/album/300139953',
+    },
+  ],
+}
+
+export default data
