0088884af9e82c656badb26408e1084aa0e308c5
Suphakit P <me@suphakit.net>
2023-07-01 13:51:57 +0700

    feat<db>: rotten buterflies

---
+++ src/pages/trees/anxio/01-rotten-butterfles/data.ts
@@ -0,0 +1,37 @@
+import DataInterface from "../../interface"
+
+const data: DataInterface = {
+  title: 'Rotten Butterflies',
+  artist: 'Anxiowaldosiaticist',
+  holder: 'JentaJuice',
+  cover: 'https://is1-ssl.mzstatic.com/image/thumb/Music116/v4/75/d0/03/75d00372-1cd9-6b34-899d-ad4eeb22f4d0/7321170657478.png/3000x3000bb.webp',
+  links: [
+    {
+      id: 0,
+      service: 'youtube',
+      link: 'https://youtu.be/F2w0OTw63K4',
+    },
+    {
+      id: 1,
+      service: 'ytmusic',
+      link: 'https://www.youtube.com/watch?v=KMxVYWzigl4',
+    },
+    {
+      id: 2,
+      service: 'spotify',
+      link: 'https://open.spotify.com/album/7np74wSfgrbA1znU0ZyORT',
+    },
+    {
+      id: 3,
+      service: 'itunes',
+      link: 'https://music.apple.com/th/album/rotten-butterflies-single/1692566842?at=1l3vpUI&ct=LFV_9b858e6e31efa52f6be977c8e543833d&itsct=catchall_p1&itscg=30440&ls=1',
+    },
+    {
+      id: 4,
+      service: 'tidal',
+      link: 'https://tidal.com/browse/album/299862474',
+    },
+  ],
+}
+
+export default data
