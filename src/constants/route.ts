b7b46e1a0d0b0a8c004808bdfc70918ae7d698e4
Suphakit P <me@suphakit.net>
2023-07-01 14:10:08 +0700

    chore<route>: add route

--- src/constants/route.ts
+++ src/constants/route.ts
@@ -1,5 +1,31 @@
 const ROUTE = {
-  HOME: "/"
+  HOME: "/",
+  AXIO: {
+    base: '/axio',
+    paths: [
+      {
+        id: 0,
+        path: '/rotten-butterflies'
+      }
+    ]
+  },
+  KATWAV: {
+    base: '/katz-wav',
+    paths: [
+      {
+        id: 7,
+        path: '/missmeyet-various'
+      },
+      {
+        id: 8,
+        path: '/with-u'
+      },
+      {
+        id: 9,
+        path: '/love-inlove-heartbroken'
+      }
+    ]
+  }
 }
 
 export default ROUTE
