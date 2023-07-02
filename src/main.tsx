7567d21b1d0705fc36584b7446044032ad56f063
Suphakit P <me@suphakit.net>
2023-07-03 02:22:23 +0700

    build: seo catching

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot, hydrateRoot } from 'react-dom/client';
 import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement!);
+
+if (rootElement?.hasChildNodes()) {
+	hydrateRoot(
+		rootElement,
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	);
+} else {
+	root.render(
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	);
+}
