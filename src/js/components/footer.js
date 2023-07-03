528f0379827b1fe60170fb263600d5afac73bfbd
Suphakit P <me@suphakit.net>
2023-07-03 13:36:50 +0700

    feat<components>: footer

---
+++ src/js/components/footer.js
@@ -0,0 +1,8 @@
+const footer = document.querySelector('footer')
+
+footer.innerHTML = `<p>
+  made w/ 🤍 by <a href="https://suphakit.net" target="_blank">Suphakit P.</a>
+</p>
+<p>
+  &copy;2023 JentaJuice. All Right Reserved.
+</p>`
